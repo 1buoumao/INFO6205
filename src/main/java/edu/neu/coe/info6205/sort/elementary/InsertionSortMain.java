452fdd69e793cf59bf54f66472d9b3bde9e0cf3c
1buoumao <chengyuxuanhtq@126.com>
2023-02-04 23:20:31 -0500

    Update InsertionSortMain.java

--- src/main/java/edu/neu/coe/info6205/sort/elementary/InsertionSortMain.java
+++ src/main/java/edu/neu/coe/info6205/sort/elementary/InsertionSortMain.java
@@ -5,44 +5,82 @@ import edu.neu.coe.info6205.util.Stopwatch;
 import edu.neu.coe.info6205.util.Timer;
 import edu.neu.coe.info6205.sort.elementary.InsertionSort;
 import java.util.Arrays;
+import java.util.zip.InflaterInputStream;
 
 public class InsertionSortMain {
     public static void main(String[] args) {
-        //initialize
-        Integer[][] tests = new Integer[5][];
-        for (int i=0;i<tests.length; i++){
-            tests[i] = new Integer[(int)(50 * Math.pow(2,i))];
-        }
-        System.out.println(tests[1].length);
-        for (int j = 0; j < tests.length; j ++ ) {
-            for (int i = 0; i < 50 * Math.pow(2,j); i++) {
-                tests[j][i] = (int) (Math.random() * 1000 * Math.pow(2, j));
-            }
-        }
-//        System.out.println(Arrays.toString(tests[1]));
-        InsertionSort<Integer> sorter = new InsertionSort<Integer>();
-        Timer timer = new Timer();
-        timer.repeat(10, () -> tests[0], x -> sorter.sort(x));
+
+        runsorts("warmup");
+        runsorts("Random array runtime at ");
+        runsorts("Sorted array runtime at ");
+        runsorts("Reversed array runtime at ");
+        runsorts("Partially sorted array runtime at ");
+    }
+
+    private static void runsorts(String typeofarrayString) {
+
+        int INITIAL_LENGTH = 200;
+        Integer[][] tests = initializearrays(typeofarrayString);
+        //run
         Double runtime = 0.0;
-//        Timer[] timers = new Timer[20];
-//        for(int i = 0; i <20; i++){
-//            timers[i] = new Timer();
-//        }
-//        for (int j = 0;j<tests.length; j ++){
-//            for (int i = 0; i < 5; i ++){
-//                int index = j;
-//                runtime = timer.repeat((int)(100* (Math.pow(2,i))), () -> tests[index], x -> sorter.sort(x));
-//                System.out.println("Random array runtime at " + 50 * Math.pow(2,i) + " times is: " + runtime);
-//            }
-//        }Stopwatch stopwatch = new Stopwatch();
+        Timer timer = new Timer();
         for (int j = 0; j < tests.length; j++){
             for (int i = 0; i < 5; i ++){
 //                sorter.sort(tests[j], 0, tests[j].length - 1);
                 int index = j;
-                runtime = timer.repeat(10, () -> tests[index], x -> new InsertionSort<Integer>().sort(x));
-                System.out.println("Random array runtime at " + 50 * Math.pow(2,i) + "on test[" + index + "]. Running" + " times is: " + runtime);
+                Integer[][] finalTests = initializearrays(typeofarrayString);
+                runtime = timer.repeat((int)(20 * Math.pow(2,j)), () -> finalTests[index], x -> new InsertionSort<Integer>().sort(x));
+                System.out.println(typeofarrayString + INITIAL_LENGTH * Math.pow(2,i) + "on test[" + index + "]. Running" + " times is: " + runtime);
             }
         }
-        
+        timer.stop();
     }
+
+    private static Integer[][] initializearrays(String typeofarrayString) {
+        //initialize
+        int INITIAL_LENGTH = 200;
+        Integer[][] tests = new Integer[5][];
+        for (int i=0;i<tests.length; i++){
+            tests[i] = new Integer[(int)(INITIAL_LENGTH * Math.pow(2,i))];
+        }
+        //System.out.println(tests[1].length);
+        for (int j = 0; j < tests.length; j ++ ) {
+            for (int i = 0; i < INITIAL_LENGTH * Math.pow(2,j); i++) {
+                tests[j][i] = (int) (Math.random() * 1000 * Math.pow(2, j));
+            }
+        }
+
+        if (typeofarrayString == "Reversed array runtime at "){
+            for (int i = 0; i < tests.length; i ++){
+                Arrays.sort(tests[i]);
+            }
+            for (int i = 0; i<tests.length; i ++){
+                int left = 0;
+                int right = tests[i].length - 1;
+                int t = 0;
+                while (left < right){
+                    t = tests[i][left];
+                    tests[i][left] = tests[i][right];
+                    tests[i][right] = t;
+                    left ++;
+                    right --;
+                }
+            }
+        }
+        if (typeofarrayString == "Sorted array runtime at "){
+            for (int i = 0; i < tests.length; i ++){
+                Arrays.sort(tests[i]);
+            }
+        }
+        if (typeofarrayString == "Partially sorted array runtime at "){
+            for (int i = 0; i<tests.length; i ++){
+                for (int j = 0; j <tests[i].length / 2; j ++){
+                    tests[i][j] = (int)(Math.random() * 1000 * Math.pow(2, i));
+                }
+            }
+        }
+        return tests;
+    }
+
 }
+
